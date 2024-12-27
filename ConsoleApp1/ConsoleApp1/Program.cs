0591946aeb670a0dab46a93fdaa2dd79a8b1ec54
rozhinarezaei <rozhinarezaei2005@gmail.com>
2024-12-27 23:04:35 +0330

    console.app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float UserPoint;
+            string point;
+            Console.WriteLine("plz enter a number");
+            point = Console.ReadLine();
+            UserPoint = int.Parse(point);
+            string res;
+            if (UserPoint < 10)
+
+            {
+                res = "failed";
+            }
+
+            else
+            {
+                res = "pass";
+            }
+            Console.WriteLine(res);
+            Console.WriteLine("press any key to exit ...");
+            Console.ReadKey();
+                    
+
+
+
+
+
+    
+        }
+    }
+}
